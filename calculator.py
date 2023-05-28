077a1661f9f637a95e8182f12abdd994a4af22b1
Maksim <homeice@mail.ru>
2023-05-28 18:15:41 +0300

    add condition

--- calculator.py
+++ calculator.py
@@ -1,59 +1,59 @@
-# 1. Напишите программу вычисления арифметического выражения заданного строкой.
-# Используйте операции +,-,/,*. приоритет операций стандартный.
+1. Напишите программу вычисления арифметического выражения заданного строкой.
+Используйте операции +,-,/,*. приоритет операций стандартный.
 
-# *Пример:*
-# 2+2 => 4;
-# 1+2*3 => 7;
-# 1-2*3 => -5;
-# - Добавьте возможность использования скобок, меняющих приоритет операций.
-# *Пример:*
-# 1+2*3 => 7;
-# (1+2)*3 => 9;
+*Пример:*
+2+2 => 4;
+1+2*3 => 7;
+1-2*3 => -5;
+- Добавьте возможность использования скобок, меняющих приоритет операций.
+*Пример:*
+1+2*3 => 7;
+(1+2)*3 => 9;
 
 # print(eval(input("Введите выражение:  "))) - быстрое решение
 
-def Summ (num1, num2):
-    return num1 + num2
-def Minus (num1, num2):
-    return num1 - num2
-def Multiplication (num1, num2):
-    return num1 * num2
-def Division (num1, num2):
-    return num1/num2
+# def Summ (num1, num2):
+#     return num1 + num2
+# def Minus (num1, num2):
+#     return num1 - num2
+# def Multiplication (num1, num2):
+#     return num1 * num2
+# def Division (num1, num2):
+#     return num1/num2
 
 
-user_string = '1-3*6'
+# user_string = '1-3*6'
 
-def user_do (user_string ):
-    res = ' '
-    for i in range (0, len(user_string)):
-        if (user_string[i] == '*') or (user_string[i] =='/'):
-            if user_string[i] == '*':
-                temp_value = str(Multiplication(int(user_string[i-1]),int(user_string[i+1])))
-                res = user_string[:i-1] + temp_value + user_string[i+2:]
-                print(res)
-                return res
-        if user_string[i] == '/':
-            temp_value = str(Division(int(user_string[i-1]),int(user_string[i+1])))
-            res = user_string[:i-1] + temp_value + user_string[i+2:]
-            print(res)
-            return res
-    for i in range (0, len(user_string)):
-        if (user_string[i] == '+') or (user_string[i] =='-'):
-            if user_string[i] == '+':
-                temp_value = str(Summ(int(user_string[i-1]),int(user_string[i+1])))
-                res = user_string[:i-1] + temp_value + user_string[i+2:]
-                print(res)
-                return res
-        if user_string[i] == '-':
-            temp_value = str(Minus(int(user_string[i-1]),int(user_string[i+1])))
-            res = user_string[:i-1] + temp_value + user_string[i+2:]
-            print(res)
-            return res
+# def user_do (user_string ):
+#     res = ' '
+#     for i in range (0, len(user_string)):
+#         if (user_string[i] == '*') or (user_string[i] =='/'):
+#             if user_string[i] == '*':
+#                 temp_value = str(Multiplication(int(user_string[i-1]),int(user_string[i+1])))
+#                 res = user_string[:i-1] + temp_value + user_string[i+2:]
+#                 print(res)
+#                 return res
+#         if user_string[i] == '/':
+#             temp_value = str(Division(int(user_string[i-1]),int(user_string[i+1])))
+#             res = user_string[:i-1] + temp_value + user_string[i+2:]
+#             print(res)
+#             return res
+#     for i in range (0, len(user_string)):
+#         if (user_string[i] == '+') or (user_string[i] =='-'):
+#             if user_string[i] == '+':
+#                 temp_value = str(Summ(int(user_string[i-1]),int(user_string[i+1])))
+#                 res = user_string[:i-1] + temp_value + user_string[i+2:]
+#                 print(res)
+#                 return res
+#         if user_string[i] == '-':
+#             temp_value = str(Minus(int(user_string[i-1]),int(user_string[i+1])))
+#             res = user_string[:i-1] + temp_value + user_string[i+2:]
+#             print(res)
+#             return res
 
-user_do(user_string)
+# user_do(user_string)
 
-user_do(user_do(user_string))
+# user_do(user_do(user_string))
 
 # import re
 
